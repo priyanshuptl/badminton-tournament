022270229d19e79a1c575d4dc450e055cd4ccbd5
Priyanshu Patel <patel.priyanshu@hotmail.com>
2019-11-17 15:37:34 +0530

    Header with title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
+      <Header title="Badminton Tournament" />
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default ({ title }) => <header className="App-header">{title}</header>;
