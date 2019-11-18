9529b0412a09ee9d42888fb5a69eed11279aa9e9
Priyanshu Patel <patel.priyanshu@hotmail.com>
2019-11-18 22:26:39 +0530

    Collapsible Panel done.

--- src/components/panel.jsx
+++ src/components/panel.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Panel = ({ title, rows }) => <div></div>;
+const Panel = ({ title, rows }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <div>
+      <button
+        className={"collapsible " + (collapsed ? "" : "active")}
+        onClick={() => setCollapsed(!collapsed)}
+      >
+        <p> {title}</p>
+        <p>{collapsed ? "+" : "-"}</p>
+      </button>
+      <div
+        className="content"
+        style={{ display: collapsed ? "none" : "block" }}
+      >
+        Rows Here!
+      </div>
+    </div>
+  );
+};
 
 export default Panel;
