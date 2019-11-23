5bae5d023173e79623f97a12a624ed3e9b8ae655
Priyanshu Patel <patel.priyanshu@hotmail.com>
2019-11-23 20:41:41 +0530

    Bracket style improved.

--- src/layout/brackets.jsx
+++ src/layout/brackets.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 import { ReactTournament } from "react-tournament";
 
+const bracketStyle = {
+  primary: {
+    default: "#717172",
+    dark: "#444446",
+    darkest: "#2b2b2b",
+    light: "#525458"
+  },
+  success: {
+    default: "#4b4b4d",
+    dark: "#3d3d3f",
+    darkest: "#282829",
+    light: "#444444"
+  },
+  fail: {
+    default: "#717172",
+    dark: "#444446",
+    darkest: "#2b2b2b",
+    light: "#525458"
+  },
+  textSmall: "12px",
+  textMedium: "16px",
+  textLarge: "22px",
+
+  textDark: "#ffffff",
+
+  textLight: "#ffffff"
+};
+
 const Brackets = ({
   data,
   showInterpoolMatches,
@@ -13,7 +41,7 @@ const Brackets = ({
       onChange={toggleShowInterpoolMatches}
     />
     Show Interpool matches
-    <ReactTournament data={data} />
+    <ReactTournament data={data} theme={bracketStyle} />
   </div>
 );
 
